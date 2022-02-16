f3ce5d7c1ea74a77b0a4c2d284beac2cbe38cba3
Leon Boschman <leon.boschman@gmail.com>
2022-02-16 22:59:31 +0100

    Added more docstrings to functions.py

--- modules/functions.py
+++ modules/functions.py
@@ -115,9 +115,19 @@ def get_info(fits_name, beam=None):
 
 
 def get_radecfreq(catalog, original):
+    """Get the right ascension, declination, and frequeny of a catalog object.
+
+    :param catalog: catalog objet header
+    :type catalog: astropy.Header? TODO check in function calls
+    :param original: name of the original file
+    :type original: str
+    :return: right ascension, declination, and frequency
+    :rtype: tuple
+    """
 
     header = fits.getheader(original)
     wcs = WCS(header)
+    # Get the x,y-position of the catalog object
     Xc = catalog['x']
     Yc = catalog['y']
     if header['NAXIS'] == 3:
@@ -130,6 +140,15 @@ def get_radecfreq(catalog, original):
 
 
 def get_subcube(source, original):
+    """Retrieve a subcube from a datacube
+
+    :param source: source object
+    :type source: Astropy Header? TODO check!
+    :param original: original data file
+    :type original: str
+    :return: subcube of data
+    :rtype: NDArray? Or Astropy dataformat? TODO check
+    """
 
     hdu_orig = fits.open(original)
 
