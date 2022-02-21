d9eca93c1b9bca6f1231728f96b3b21a8e16420f
Kelley M. Hess <hess@astro.rug.nl>
2022-02-21 17:01:38 +0100

    Use image from first listed survey in combined figure (#11)

--- src/combine_images.py
+++ src/combine_images.py
@@ -2,7 +2,7 @@ import os
 
 
 # Note, although this has been generalized, it seems to work best with png's!
-def combine_images(source, src_basename, suffix='png'):
+def combine_images(source, src_basename, suffix='png', surveys='DSS2 Blue'):
 
     # Specify the command to use imagemagick's convert (karma has a convert which may conflict)
     # convert_im = "/usr/local/Cellar/imagemagick/7.1.0-13/bin/convert"
@@ -14,8 +14,8 @@ def combine_images(source, src_basename, suffix='png'):
     # Use terminal commands to assemble figures with imagemagick: https://imagemagick.org/index.php
     print("\n\tAssembling figures with imagemagck for source {}: {}.".format(source['id'], source['name']))
     new_file = "{}combo.{}".format(infile, suffix)
-    os.system("{0} {1}mom0dss2blue.{2} {1}mom0hi.{2} {1}snr.{2} {1}mom1.{2}"
-              " +append temp.{2}".format(convert_im, infile, suffix))
+    os.system("{0} {1}mom0{3}.{2} {1}mom0hi.{2} {1}snr.{2} {1}mom1.{2}"
+              " +append temp.{2}".format(convert_im, infile, suffix, surveys[0].replace(" ", "").lower()))
     os.system("{0} {1}spec.{2} -resize 125% temp2.{2}".format(convert_im, infile, suffix))
     os.system("{0} {1}specfull.{2} -resize 125% temp3.{2}".format(convert_im, infile, suffix))
     os.system("{0} temp2.{2} temp3.{2} {1}pv.{2} +append temp4.{2}".format(convert_im, infile, suffix))
