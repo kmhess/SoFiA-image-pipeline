67ea6cda74f3fa874cbfd37f0dac51401de63bb6
Kelley M. Hess <hess@astro.rug.nl>
2022-05-06 14:30:34 +0200

    Update versioning

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='sofia_image_pipeline',
-    version='0.0.1',
+    version='1.1.0',
     description='SIP takes a SoFiA generated source catalog and produce images for publication or quick inspection. Images include HI contours overlaid on multiwavelength images, HI moment maps, pixel-by-pixel SNR maps, pv-diagrams with SoFiA mask, and spectra with and without noise.',
     url='https://github.com/kmhess/SoFiA-image-pipeline',
-    author='Kelley Hess',
+    author='Kelley M. Hess',
     author_email='kmhess.astro@gmail.com',
     packages=[
         "src",
