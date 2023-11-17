7c99a51a36ea24895cb05186b18697e0d04aec3e
Kelley M. Hess <kmhess.astro@gmail.com>
2023-11-17 11:23:51 +0100

    Update required astropy & pillow versions for bug fix & security

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sofia_image_pipeline',
-    version='1.2.3',
+    version='1.2.4',
     description='SIP takes a SoFiA generated source catalog and produce images for publication or quick inspection. Images include HI contours overlaid on multiwavelength images, HI moment maps, pixel-by-pixel SNR maps, pv-diagrams with SoFiA mask, and spectra with and without noise.',
     url='https://github.com/kmhess/SoFiA-image-pipeline',
     author='Kelley M. Hess',
@@ -12,14 +12,14 @@ setup(
         "src.modules",
     ],
     install_requires=[
-        "astropy == 5.0.1",
+        "astropy >= 5.0.2",
         "astroquery == 0.4.5",
         "matplotlib == 3.4.1",
         "numpy == 1.22.0",
-        "Pillow == 9.2.0",
+        "Pillow >= 10.0.0",
         "pvextractor >= 0.2",
         "requests == 2.25.1",
-        "setuptools==62.3.2",
+        "setuptools == 62.3.2",
         "sphinx_rtd_theme == 1.0.0",
         "xmltodict == 0.12.0"
     ],
